a98ba3771822230c795ef06c81c6e46fb6f04ca4
Quy Cao <quy.caoquoc@gmail.com>
2026-05-26 23:45:46 +0700

    Remove legacy wall_* files; migrate to home_*

--- lib/features/home/domain/wall_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:read_radius/features/home/domain/wall_book.dart';
-import 'package:read_radius/features/home/domain/wall_book_details.dart';
-
-abstract class WallRepository {
-  Future<List<WallBook>> searchBooks(String query);
-  Future<List<WallBook>> fetchTrendingBooks();
-  Future<WallBookDetails> fetchBookDetails(String bookId);
-}
